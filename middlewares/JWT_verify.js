4611b8bcd10b3bb0c1434e32a25bb466beaecd23
jawahar112 <bharathijawahar583@gmail.com>
2024-07-31 10:22:07 +0530

    updates

--- middlewares/JWT_verify.js
+++ middlewares/JWT_verify.js
@@ -1,11 +1,17 @@
 import { VerifyToken } from "../helpers/Jwt_helper.js";
 export const VerifyUser=async(req,res,next)=>{
-    const Token=req.headers.authorization
-    const User=await VerifyToken(Token)
-    if(!Token){
-        return res.status(400).json({Message:"Token Must Be Provided",status:false})
+    try {
+        const Token=req.headers.authorization
+        const User=await VerifyToken(Token)
+        if(!Token){
+            return res.status(400).json({Message:"Token Must Be Provided",status:false})
+        }
+        req.UserId=User.UserId
+       
+        next()
+    } catch (error) {
+        return res
+        .status(500)
+        .json({ Error: "Internal Server Error", message: error.message });
     }
-    req.UserId=User.UserId
-   
-    next()
 }
--- middlewares/Validations/Joi/Uservalidations.js
+++ middlewares/Validations/Joi/Uservalidations.js
@@ -1,14 +1,25 @@
-
 import { LoginSchema,RegisterSchema } from "../../../utils/Joi_Schema/UserSchema.js";
 export const ValidateLogin=(req,res,next)=>{
 
-    const {error}=LoginSchema.validate(req.body)
-    if(error) return res.json({Message:error.message,status:false})  
-        next() 
+ try {
+       const {error}=LoginSchema.validate(req.body)
+       if(error) return res.json({Message:error.message,status:false})  
+           next() 
+ } catch (error) {
+    return res
+    .status(500)
+    .json({ Error: "Internal Server Error", message: error.message });
+ }
 }
 export const ValidateRegister=(req,res,next)=>{
     
-    const{error}=RegisterSchema.validate(req.body)
-    if(error) return res.json({Message:error.message,status:false})
-        next()  
+   try {
+     const{error}=RegisterSchema.validate(req.body)
+     if(error) return res.json({Message:error.message,status:false})
+         next()
+   } catch (error) {
+    return res
+    .status(500)
+    .json({ Error: "Internal Server Error", message: error.message });
+   }  
 }
